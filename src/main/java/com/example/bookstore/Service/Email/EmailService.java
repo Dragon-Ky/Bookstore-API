793290793ef41580bd340c2dba035d0f3bd62cc6
Dragon-Ky <giaky200601@gmail.com>
2026-03-29 14:55:29 +0700

    fix service

--- src/main/java/com/example/bookstore/Service/Email/EmailService.java
+++ src/main/java/com/example/bookstore/Service/Email/EmailService.java
@@ -1,8 +1,6 @@
 package com.example.bookstore.Service.Email;
 
-import org.springframework.stereotype.Service;
 
-@Service
 public interface EmailService {
     void sendVerificationEmail(String to, String otp);
 }
