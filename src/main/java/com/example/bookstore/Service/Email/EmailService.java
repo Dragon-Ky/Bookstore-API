8b43678d587c8c6571fed2d446615f2c72af9dc9
Dragon-Ky <giaky200601@gmail.com>
2026-03-29 14:27:03 +0700

    feat: add framework service

--- src/main/java/com/example/bookstore/Service/Email/EmailService.java
+++ src/main/java/com/example/bookstore/Service/Email/EmailService.java
@@ -1,4 +1,7 @@
 package com.example.bookstore.Service.Email;
+
+import org.springframework.stereotype.Service;
+
 @Service
 public interface EmailService {
     void sendVerificationEmail(String to, String otp);
