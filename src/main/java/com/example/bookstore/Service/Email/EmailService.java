62432e5035b08b726ac72ea4918e0127d27e1506
Dragon-Ky <giaky200601@gmail.com>
2026-03-29 12:02:04 +0700

    feat: implement user authentication, email verification service, and database configuration

---
+++ src/main/java/com/example/bookstore/Service/Email/EmailService.java
@@ -0,0 +1,4 @@
+package com.example.bookstore.Service.Email;
+
+public interface EmailService {
+}
---
+++ src/main/java/com/example/bookstore/Service/Email/GmailEmailService.java
@@ -0,0 +1,4 @@
+package com.example.bookstore.Service.Email;
+
+public class GmailEmailService {
+}
